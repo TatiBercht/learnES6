438521e6863f00142796990349d3e5a6095e3839
Tati Bercht <taty.rb@gmail.com>
2017-03-13 20:47:14 +0000

    second lesson

--- test/intro/chp1.js
+++ test/intro/chp1.js
@@ -50,52 +50,84 @@ describe('intro', () => {
   });
 
   describe('function parameters', () => {
+    let result;
+
     function foo(x = 11, y = 31) {
       return x + y;
     }
 
-    it('foo() == ?', () => {
-      // expect(foo()).to.equal(?);
+    it('foo() == 42', () => {
+      result = foo();
+      expect(result).to.equal(42);
     });
 
-    it('foo(5, 6) == ?', () => {
-      // expect(foo(5, 6)).to.equal(?);
+    it('foo(5, 6) == 11', () => {
+      result = foo(5, 6);
+      expect(result).to.equal(11);
     });
 
-    it('foo(0, 42) == ?', () => {
-      // expect(foo(0, 42)).to.equal(?);
+    it('foo(0, 42) == 42', () => {
+      expect(foo(0, 42)).to.equal(42);
     });
 
-    it('foo(5) == ?', () => {
-      // expect(foo(5)).to.equal(?);
+    it('foo(5) == 36', () => {
+      expect(foo(5)).to.equal(36);
     });
 
-    it('foo(5, undefined) == ?', () => {
-      // expect(foo(5, undefined)).to.equal(?);
+    it('foo(5, undefined) == 36', () => {
+      expect(foo(5, undefined)).to.equal(36);
     });
 
-    it('foo(5, null) == ?', () => {
-      // expect(foo(5, null)).to.equal(?);
+    it('foo(5, null) == 5', () => {
+      expect(foo(5, null)).to.equal(5);
     });
 
-    it('foo(null, 5) == ?', () => {
-      // expect(foo(null, 5)).to.equal(?);
+    it('foo(null, 5) == 5', () => {
+      expect(foo(null, 5)).to.equal(5);
     });
 
-    it('foo(undefined, 5) == ?', () => {
-      // expect(foo(undefined, 5)).to.equal(?);
+    it('foo(undefined, 5) == 16', () => {
+      expect(foo(undefined, 5)).to.equal(16);
     });
   });
 
   describe('immediately executed functions', () => {
-    function foo(x = (function (v) { // eslint-disable-line wrap-iife
+    function myFunc(v) {
       return v + 11;
-    })(31)) {
+    }
+
+    // let myFunc = (v) => {
+    //   return v + 11;
+    // };
+
+    function foo(x = myFunc(31)) {
       return x * 2;
     }
 
-    it('foo() == ?', () => {
-      // expect(foo()).to.equal(?);
+    it('myFunc(31) = 42', () => {
+      expect(myFunc(31)).to.equal(42);
+    });
+
+    it('foo() == 84', () => {
+      expect(foo()).to.equal(84);
+    });
+
+    it('foo(10) == 20', () => {
+      expect(foo(10)).to.equal(20);
+    });
+  });
+
+  describe('more functions', () => {
+    function myFunc(x) {
+      return 2 * x;
+    }
+
+    it('myFunc(2) == 4', () => {
+      expect(myFunc(2)).to.equal(4);
+    });
+
+    it('myFunc() isNaN', () => {
+      expect(myFunc()).isNaN; // eslint-disable-line no-unused-expressions
     });
   });
 });
