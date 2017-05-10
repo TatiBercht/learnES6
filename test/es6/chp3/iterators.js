28669e6277d7011d05bb7a5622e16ec1918210bc
garyalway@gmail.com <Gary.Alway@uk.tesco.com>
2017-05-10 13:06:43 +0100

    map iterators

--- test/es6/chp3/iterators.js
+++ test/es6/chp3/iterators.js
@@ -1,4 +1,4 @@
-describe('Iterators', () => {
+describe('Array Iterators', () => {
   const arr = [1, 2, 3];
   const itSymbol = arr[Symbol.iterator]();
 
@@ -16,5 +16,26 @@ describe('Iterators', () => {
 
   it('Symbol.iterator final .next() function', () => {
     expect(itSymbol.next()).to.deep.equal({ value: undefined, done: true });
+  });
+});
+
+describe('Map Iterators', () => {
+  const m = new Map();
+
+  m.set('foo', 42);
+  m.set({ cool: true }, 'hello world');
+
+  const it1 = m[Symbol.iterator]();
+
+  it('Symbol.iterator first .next() function', () => {
+    expect(it1.next()).to.deep.equal({ value: ['foo', 42], done: false });
+  });
+
+  it('Symbol.iterator second .next() function', () => {
+    expect(it1.next()).to.deep.equal({ value: [{ cool: true }, 'hello world'], done: false });
+  });
+
+  it('Symbol.iterator final .next() function', () => {
+    expect(it1.next()).to.deep.equal({ value: undefined, done: true });
   });
 });
