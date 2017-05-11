3bf2f5496a95a7d5454bde006724b469a2612e21
garyalway@gmail.com <Gary.Alway@uk.tesco.com>
2017-05-11 15:06:02 +0100

    generator force return

--- test/es6/chp3/generators.js
+++ test/es6/chp3/generators.js
@@ -49,3 +49,25 @@ describe('Generator eg 3', () => {
     expect(iterator.next()).to.deep.equal({ value: undefined, done: true });
   });
 });
+
+describe('Generator eg 4 - .return forces exhaution of a generator', () => {
+  function* foo() {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+
+  const iterator = foo();
+
+  it('first call to generator', () => {
+    expect(iterator.next()).to.deep.equal({ value: 1, done: false });
+  });
+
+  it('force return call to generator', () => {
+    expect(iterator.return(22)).to.deep.equal({ value: 22, done: true });
+  });
+
+  it('force return has exhausted the generator', () => {
+    expect(iterator.next()).to.deep.equal({ value: undefined, done: true });
+  });
+});
